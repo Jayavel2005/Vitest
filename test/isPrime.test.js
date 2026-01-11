5c5b761797ef0a2d636c54713826e55d18eff5e0
Jayavel <jayavel1520@gmail.com>
2026-01-11 21:32:19 +0530

    test: cover edge cases, types, and error handling for isPrime

--- test/isPrime.test.js
+++ test/isPrime.test.js
@@ -1,2 +1,38 @@
-import {describe, test, expectTypeOf} from "vitest";
+import {describe, test, expect} from "vitest";
 import {isPrime} from "../src/isPrime.js";
+
+
+describe("isPrime", ()=>{
+    test("returns true/truthy for small prime numbers", ()=>{
+        expect(isPrime(5)).toBe(true);
+        expect(isPrime(7)).toBeTruthy();
+    })
+
+    test("returns false/falsy for non-prime numbers", ()=>{
+        expect(isPrime(6)).toBe(false);
+        expect(isPrime(10)).toBeFalsy();
+    })
+
+    test("matches results in an array using toEqual", ()=>{
+        const results = [1,2,3].map(num => isPrime(num));
+        expect(results).toEqual([false, true, true]);
+    })
+
+    test("detects primes within a filtered list using contains", ()=>{
+        const nums = [1,2,3,4,5,6,7];
+        const filter = nums.filter(num => isPrime(num));
+
+        expect(filter).toContain(5);
+        expect(filter).not.toContain(8);
+    })
+
+    test("throws an error when passed s non-number", ()=>{
+        expect(()=>isPrime('pika')).toThrow("Input must be a number")
+    })
+
+    test("has correct type for result", ()=>{
+        expect(isPrime(5)).toBeTypeOf("boolean");
+        expect(typeof isPrime(4)).toBe("boolean");
+
+    })
+})
