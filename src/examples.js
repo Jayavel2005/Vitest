6f5c0dd6fec67b40d2090d49a75d425d82d4235c
Jayavel <jayavel1520@gmail.com>
2026-01-11 20:50:22 +0530

    feat: Unit testing with a function called longest string between two strings.

--- src/examples.js
+++ src/examples.js
@@ -1,3 +1,3 @@
 export const longestString = (str1, str2) =>{
-    return str1.length >= str2.length ? str1 : str2;
+    return str1.trim().length >= str2.trim().length ? str1 : str2;
 }
--- test/longestString.test.js
+++ test/longestString.test.js
@@ -1,7 +1,28 @@
-import {describe,it, expect} from "vitest";
-import {longestString} from "../src/examples.js";
-describe("longestString", ()=>{
-    it("returns the  longest length String", ()=>{
-        expect(longestString("Java", "coder")).toBe("coder")
-    })
-})
+import { describe, it, expect, test } from "vitest";
+import { longestString } from "../src/examples.js";
+
+describe("longestString", () => {
+
+    it("returns the longest string", () => {
+        expect(longestString("Java", "coder")).toBe("coder");
+    });
+
+    it("returns first string if both have same length", () => {
+        expect(longestString("pikachu", "snorlax")).toBe("pikachu");
+    });
+
+    test("handles empty strings", () => {
+        expect(longestString("", "mario")).toBe("mario");
+        expect(longestString("luigi", "")).toBe("luigi");
+        expect(longestString("", "")).toBe("");
+    });
+
+    it("handles strings with spaces", () => {
+        expect(longestString("", "")).toBe("");
+    });
+
+    it('should handles the leading or trailing spaces', () => {
+        expect(longestString("     mist       ", "ashKetch")).toBe("ashKetch");
+    });
+
+});
